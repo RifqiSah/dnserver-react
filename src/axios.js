92799427f7022552a2bc4218356e89584b62a313
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2023-03-19 23:43:44 +0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://api.rifqisah.com/v1/aisha'
+    baseURL: 'https://api.rifqisah.com/v1/aisha',
+    headers: {
+        apikey: process.env.API_KEY,
+    },
 });
 
 export default instance;
